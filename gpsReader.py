a1e6f7126dd3edf88209291e740e8d0b2623636a
Nordine Sebkhi <sebnor31@hotmail.com>
2016-06-15 17:17:02 -0400

    Derive bearing from lat and lon in GPS

--- gpsReader.py
+++ gpsReader.py
@@ -1,12 +1,12 @@
 #! /usr/bin/python
 # Author: Nordine Sebkhi
 
-import os
 from gps import *
 from time import *
 import time
 import threading
 from datetime import datetime
+from math import cos, sin, radians, degrees, atan2
 
 
 class GpsReader(threading.Thread):
@@ -21,6 +21,8 @@ class GpsReader(threading.Thread):
         self.gpsPoller.start()
 
         gpsFound = False
+        prevLat = 0
+        prevLon = 0
 
         while True:
             gpsData = self.gpsPoller.getCurrentValue()
@@ -38,34 +40,43 @@ class GpsReader(threading.Thread):
                         ts.year, ts.month, ts.day, ts.hour, ts.minute, ts.second)
 
                     with open(gpsFile, 'w') as f:
-                        f.write('LAT,LON,ALT,SPEED,CLIMB,GPS_TIME,RPI_TIME\n')
+                        f.write('LAT,LON,BEARING,ALT,SPEED,CLIMB,GPS_TIME,RPI_TIME\n')
 
                 # Append gps data
-                timeGPS = str(gpsData.time)
-                lat = str(gpsData.lat)        # Degrees (North:+ South:-)
-                lon = str(gpsData.lon)        # Degrees (East:+  West:-)
-                alt = str(gpsData.alt)        # Meters
-                speed = str(gpsData.speed)    # Meters per second
-                climb = str(gpsData.climb)    # Climb(+) or Sink(-) in meters per second
+                timeGPS = gpsData.time
+                lat = gpsData.lat        # Degrees (North:+ South:-)
+                lon = gpsData.lon        # Degrees (East:+  West:-)
+                alt = gpsData.alt        # Meters
+                speed = gpsData.speed    # Meters per second
+                climb = gpsData.climb    # Climb(+) or Sink(-) in meters per second
+
+                if (prevLat == 0):
+                    bearing = 0
+                else:
+                    bearing = self.getBearing(prevLat, prevLon, lat, lon)
 
                 with open(gpsFile, 'a') as f:
-                    f.write('{0},{1},{2},{3},{4},{5},{6}\n'.format(lat, lon, alt, speed, climb, timeGPS, ts))
+                    f.write('{0},{1},{2},{3},{4},{5},{6},{7}\n'.format(
+                        lat, lon, bearing, alt, speed, climb, timeGPS, ts))
 
-                # Print data
-                #os.system('clear')
-                #print(gpsData)
-                #print
-                #print ' GPS reading'
-                #print '----------------------------------------'
-                #print 'time        ', gpsData.time
-                #print 'latitude    ', gpsData.lat
-                #print 'longitude   ', gpsData.lon
-                #print 'altitude (m)', gpsData.alt
-                #print 'speed (m/s) ', gpsData.speed
-                #print 'climb       ', gpsData.climb
-                #print 'mode        ', gpsData.mode
+                # prepare for next iteration
+                prevLat = lat
+                prevLon = lon
+                time.sleep(0.5)
 
-                time.sleep(0.5)      # set to whatever
+    def getBearing(self, lat_start_deg, lon_start_deg, lat_end_deg, lon_end_deg):
+        latStart = radians(lat_start_deg)
+        lonStart = radians(lon_start_deg)
+        latEnd = radians(lat_end_deg)
+        lonEnd = radians(lon_end_deg)
+
+        d_Lon = lonEnd - lonStart
+
+        x = cos(latEnd) * sin(d_Lon)
+        y = cos(latStart) * sin(latEnd) - sin(latStart) * cos(latEnd) * cos(d_Lon)
+
+        bearingRad = atan2(x, y)
+        return degrees(bearingRad)
 
 
 class GpsPoller(threading.Thread):
@@ -85,3 +96,16 @@ class GpsPoller(threading.Thread):
 
         except StopIteration:
             pass
+
+##################################
+# Test
+#############################
+if __name__ == "__main__":
+    latStart = 39.099912
+    lonStart = -94.581213
+    latEnd = 38.627089
+    lonEnd = -90.200203
+
+    gpsReader = GpsReader("don't care'")
+    bearing = gpsReader.getBearing(latStart, lonStart, latEnd, lonEnd)
+    print("Bearing (deg) = {0:f}".format(bearing))
