5aa417385f5828f74baac0071468701a6ab574a9
Nordine Sebkhi <sebnor31@hotmail.com>
2016-06-20 15:27:32 -0400

    Use MAC address as a unique ID and create new folder structure to save data

--- mainDataCapture.py
+++ mainDataCapture.py
@@ -2,21 +2,46 @@ from motionReader import MotionReader
 from videoReader import VideoReader
 from gpsReader import GpsReader
 from subprocess import call
+from datetime import datetime
+import os
 
-# Dir where data will be saved
-# Make sure it also corresponds to the mount dir
-outputDir = "/mnt/wheego/Data/"
 
 # Mount Falsh drive to wheego folder
 command = 'sudo mount -o uid=pi,gid=pi /dev/sda1 /mnt/wheego'
 call(command, shell=True)
 
+# Root directory where data will be saved
+# Make sure it also corresponds to the mount dir
+rootDir = "/mnt/wheego/Data"
+
+if not os.path.exists(rootDir):
+    os.makedirs(rootDir)
+
+# Read MAC address (et0) as unique ID of device
+macAddr = ''
+with open('/sys/class/net/eth0/address') as f:
+    macAddr = f.read().rstrip('\n')
+    macAddr = macAddr.replace(":", "")  # Cannot create folder name that includes ":"
+
+macDir = "{0}/{1}".format(rootDir, macAddr)
+
+if not os.path.exists(macDir):
+    os.makedirs(macDir)
+
+# Dir witch current time stamp
+ts = datetime.now()
+outputDir = macDir + "/{0}-{1}-{2}_{3}-{4}-{5}/".format(
+                        ts.year, ts.month, ts.day, ts.hour, ts.minute, ts.second, macAddr)
+
+if not os.path.exists(outputDir):
+    os.makedirs(outputDir)
+    
 # Launch data capture threads
 motionReader = MotionReader(outputDir)
 motionReader.start()
 
-videoReader = VideoReader(outputDir)
-videoReader.start()
+#videoReader = VideoReader(outputDir)
+#videoReader.start()
 
 gpsReader = GpsReader(outputDir)
 gpsReader.start()
